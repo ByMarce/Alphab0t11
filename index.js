2bb13ef325b70c64f7814d55c2b88a28cfe5716b
ByMarce <94191730+ByMarce@users.noreply.github.com>
2021-11-24 04:08:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -193,8 +193,8 @@ autoketik = false
 autoregister = setting.user_register
 
 img = setting.img
-baper = 'SUB YT ZEEONE OFC'
-apiku = 'https://youtu.be/2rWbjrDIAyA'
+baper = 'HOLA BUENAS'
+apiku = 'https://wa.me/51936366761'
 gc_wa_lu = 'https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
 targetpc = setting.ownerNumberr
 owner = targetpc
